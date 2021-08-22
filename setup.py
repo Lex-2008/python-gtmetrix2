c8cc04a17c2a4ff03144bb25f30afb5b6c904733
Alexey Shpakovsky <alexey@shpakovsky.ru>
2021-08-22 22:28:04 +0200

    Limited PyPI deployment to use only Python 3.9
    Also automated getting of version

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Note that $TRAVIS_TAG is always "set" on Travis, it just might be ""
+version = os.environ.get('TRAVIS_TAG','')
+if version == '':
+    version = '0.'+os.environ.get('TRAVIS_BUILD_NUMBER','0')
+
+print(version)
+
 setuptools.setup(
     name="python-gtmetrix2",
-    version="0.0.1",
+    version=version,
     author="Alexey Shpakovsky",
     author_email="alexey+setup.py@shpakovsky.ru",
     description="A Python client library for GTmetrix REST API v2.0",
