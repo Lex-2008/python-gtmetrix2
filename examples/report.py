48ada0e56d79f8f18733a54f0f14e1c8910e4e1b
Alexey Shpakovsky <alexey@shpakovsky.ru>
2021-09-02 09:58:58 +0200

    add primitive har parsing to report exampe

--- examples/report.py
+++ examples/report.py
@@ -24,21 +24,32 @@ def main(api_key, report_id, operation="print", *args):
             print("If filename is not provided, resource is printed to stdout.")
             exit()
         getresource(report, *args)
+    elif operation == "size":
+        har = json.loads(report.getresource("net.har").decode())
+        size_bytes = summarizeHar(har)
+        size_kb = size_bytes/1024
+        size_mb = size_kb/1024
+        print("Total size of all resources, uncompressed: %d bytes = %.1f kb = %.1f MB" % (size_bytes, size_kb, size_mb))
     else:
         print("Usage: %s api_key report_id [operation]" % sys.argv[0])
         print("or: %s api_key report_id getresource resource [filename]" % sys.argv[0])
-        print("where operation is one of: print (default), delete, retest, getresource")
+        print("where operation is one of: print (default), delete, retest, size, getresource")
 
 
 def getresource(report, resource, filename=sys.stdout.buffer):
     report.getresource(resource, filename)
 
 
+def summarizeHar(har):
+    """Given a har file (parsed json object), returns total size of all responses, in bytes."""
+    return sum((entry["response"]["content"]["size"] for entry in har["log"]["entries"]))
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: %s api_key report_id [operation]" % sys.argv[0])
         print("or: %s api_key report_id getresource resource [filename]" % sys.argv[0])
-        print("where operation is one of: print (default), delete, retest, getresource")
+        print("where operation is one of: print (default), delete, retest, size, getresource")
         print("getresource operation requires one extra argument: what resource to get,")
         print("and one optional: filename where to save it.")
         print("If filename is not provided, resource is printed to stdout.")
